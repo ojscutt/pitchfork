9a7fc15adb9c5e749f4dfa7bff26e01b545ba8e3
owen <oxs235@student.bham.ac.uk>
2025-10-02 15:59:42 +0100

    pre-wtf fix push

---
+++ scripts/__init__.py
@@ -0,0 +1,3 @@
+#from .tf_to_dict import tf_to_dict
+from .wtf_compile import jax_compile, numpy_compile
+from .prior_funcs import custom_uniform_prior, custom_beta_prior
--- wtf/__init__.py
+++
@@ -1,2 +0,0 @@
-from .tf_to_dict import tf_to_dict
-from .wtf_compile import wtf_compile
